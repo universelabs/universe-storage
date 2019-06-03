8e0bfc7bca487c668aa69684a7df4ebbcc3c2e05
Jacob <jacobgadikian@gmail.com>
2019-06-03 13:48:02 -0400

    Commented out Ethereum stuff until can figure out what the build issue is.
    Now generating Bitcoin HD keys, still need to resolve that to addresses.
    From my understanding, HD keys have nearly unlimited addresses, but I
    need to develop a better understanding.

--- main.go
+++ main.go
@@ -2,21 +2,45 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/asdine/storm"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil/hdkeychain"
 )
 
 func main() {
 
-	//Ethereum Private Key
-	privateKey, err := crypto.GenerateKey()
+	//Test cause ethereum address wasn't printing
+	fmt.Println("Hello giant chicken")
+
+	//Generate Bitcoin address  -- From: https://godoc.org/github.com/btcsuite/btcutil/hdkeychain#example-NewMaster
+	// Generate a random seed at the recommended length.
+	seed, err := hdkeychain.GenerateSeed(hdkeychain.RecommendedSeedLen)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		return
 	}
 
-	fmt.Println(privateKey)
+	// Generate a new master node using the seed.
+	key, err := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Show that the generated master node extended key is private.
+	fmt.Println("Private Extended Key?:", key.IsPrivate())
+	fmt.Println(key)
+
+	//Generate Ethereum Address
+	/*
+		privateKey, err := crypto.GenerateKey()
+		if err != nil {
+			log.Fatal(err)
+		}
+			fmt.Print(privateKey)
+
+	*/
 
 	// Storage
 	db, err := storm.Open("my.db")
