eb792b38af2d3f931dec195512ebdd000c4fc36a
Itamar Reif <itamar.reif@gmail.com>
2019-06-10 10:18:29 -0400

    dirty code, having trouble running this on WSL

--- keystore/keystore.go
+++ keystore/keystore.go
@@ -1,8 +1,10 @@
-package main
+package storage
 
 import (
 	"fmt"
 	"log"
+	// "os"
+	// "bufio"
 	"github.com/asdine/storm"
 )
 
@@ -10,45 +12,116 @@ type Keystore struct {
 	db *storm.DB
 }
 
-type WalletID struct {
-	ID int 'storm:"unique, increment"' // uid for stormdb
-	Platform string 'storm:"index"' // e.g. ETH, BTC, Blockstack
-	Description string 'storm:"index"' // e.g. BTC:personal, ETH:app1, arbitrary descriptor
+type Key interface {
+	IsKey()
 }
 
 type Wallet struct {
-	PublicKey string 
-	PrivateKey string 
-	Metadata WalletID 'storm:"inline"'
+	ID int `storm:"id,increment"` // uid for stormdb
+	Platform string `storm:"index"` // e.g. ETH, BTC, Blockstack
+	Description string `storm:"index"` // e.g. BTC:personal, ETH:app1, arbitrary descriptor
+
+	Data Key // 
 }
 
-func main() {
-	var keystore *Keystore
-	keystore.db, err := storm.Open("keys.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer keystore.db.Close()
-	
-	// make a few wallet structs for testing
+type BlockstackID struct {
+	Email string
+	Password string
+	Passphrase string 
+}
 
-	// TODO: keystore_test.go
+func (b BlockstackID) IsKey() {}
+
+type ETHKey struct {
+	PublicKey string
+	PrivateKey string
+}
+
+func (b ETHKey) IsKey() {}
+
+type BTCKey struct {
+	PublicKey string
+	PrivateKey string 
+}
+
+func (b BTCKey) IsKey() {}
+
+
+// func main() {
+// 	keystore := Keystore{}
+// 	err := keystore.Init("db.db")
+// 	// db, err := storm.Open("db.db")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer keystore.Close()
+// 	// make a few wallet structs for testing
+// 	eth1 := Wallet{
+// 		Platform: "eth",
+// 		Description: "test1",
+// 		Data: ETHKey{
+// 			PublicKey: "wiq73yrh79yr9rf93hfyca",
+// 			PrivateKey: "fgbosfgnuonoufnduonf3f3o",},
+// 	}
+// 	fmt.Println(eth1)
+// 	saveerr := keystore.AddWallet(&eth1)
+// 	if saveerr != nil {
+// 		fmt.Println(saveerr)
+// 	} else {
+// 		fmt.Printf("saved eth1:%v\n", eth1)
+// 	}
+// 	ret, reterr := keystore.GetPlatform("eth")
+// 	if reterr != nil {
+// 		fmt.Println(reterr)
+// 	} else {
+// 		fmt.Println(ret)
+// 	}
+// 	// TODO: keystore_test.go
+// }
+
+// keystore methods
+
+func (keystore *Keystore) Init(pathname string) error {
+	var err error
+	keystore.db, err = storm.Open(pathname)
+	return err
+}
+
+func (keystore *Keystore) Close() error {
+	err := keystore.db.Close()
+	return err
 }
 
 // CRUD interface
 
-func (keystore *Keystore) AddWallet() err {
-	
+func (keystore *Keystore) AddWallet(new_wallet *Wallet) error {
+	err := keystore.db.Save(new_wallet)
+	return err
 }
 
-func (keystore *Keystore) RemoveWallet() err {
-	
+func (keystore *Keystore) GetWallet(id int) (Wallet, error) {
+	var wallet Wallet
+	err := keystore.db.One("ID", id, &wallet)
+	return wallet, err
 }
 
-func (keystore *Keystore) UpdateWallet() err {
-	
+func (keystore *Keystore) GetPlatform(platform string) ([]Wallet, error) {
+	var wallets []Wallet
+	err := keystore.db.Find("Platform", platform, &wallets)
+	return wallets, err
 }
 
-func (keystore *Keystore) DeleteWallet() err {
-	
+func (keystore *Keystore) GetAll() ([]Wallet, error) {
+	var wallets []Wallet
+	err := keystore.db.All(&wallets)
+	return wallets, err
 }
+
+// func (keystore Keystore) UpdateWallet(id int, new_wallet *Wallet) error {
+// 	err := keystore.db.UpdateField()
+// 	return err
+// }
+
+// func (keystore Keystore) DeleteWallet() error {
+	
+// }
