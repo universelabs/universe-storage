2075cf0ce1a6d68817c5d5ca72a96467050c06bb
Itamar Reif <itamar.reif@gmail.com>
2019-06-24 18:46:35 +0300

    reorganized

---
+++ keygen/keygen.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+
+	"github.com/asdine/storm"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil/hdkeychain"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/sha3"
+)
+
+func main() {
+
+	//Generate Bitcoin address  -- From: https://godoc.org/github.com/btcsuite/btcutil/hdkeychain#example-NewMaster
+	// Generate a random seed at the recommended length.
+	seed, err := hdkeychain.GenerateSeed(hdkeychain.RecommendedSeedLen)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Generate a new master node using the seed.
+	key, err := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Show that the generated master node extended key is private.
+	fmt.Println("Private Extended Key?:", key.IsPrivate())
+	bitcoinprivatekey, err := key.ECPrivKey()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Your HD extended private key:", key)
+	fmt.Println("An EC private key derived from your HD private key:", bitcoinprivatekey)
+
+	//Generate Ethereum Address -- From: https://goethereumbook.org/wallet-generate/
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	fmt.Println(hexutil.Encode(privateKeyBytes)[2:]) // 0xfad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	fmt.Println(hexutil.Encode(publicKeyBytes)[4:]) // 0x049a7df67f79246283fdc93af76d4f8cdd62c4886e8cd870944e817dd0b97934fdd7719d0810951e03418205868a5c1b40b192451367f28e0088dd75e15de40c05
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	fmt.Println(address) // 0x96216849c49358B10257cb55b28eA603c874b05E
+
+	hash := sha3.NewKeccak256()
+	hash.Write(publicKeyBytes[1:])
+	fmt.Println(hexutil.Encode(hash.Sum(nil)[12:])) // 0x96216849c49358b10257cb55b28ea603c874b05e
+
+	// Storage
+	db, err := storm.Open("my.db")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+}
